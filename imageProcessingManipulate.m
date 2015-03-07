ff760b9f16aaf79eed2b9681260f48ff4b52068a
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-07 18:43:20 -0500

    Updated Mathematica scripts

--- imageProcessingManipulate.m
+++ imageProcessingManipulate.m
@@ -9,7 +9,7 @@ Manipulate[
 	RemoveBackground[
 	ColorNegate[
 	EdgeDetect[
- Import["/Users/Pradyuman/Documents/LiveTrace/purdue_campus.jpg"],r,t]], {"Background", color}],{{r,2,"radius"},1,10},{{t,.1,"threshold"}, 0, .5}]
+ Import["/Users/Pradyuman/Documents/LiveTrace/images/purdue_campus.jpg"],r,t]], {"Background", color}],{{r,2,"radius"},1,10},{{t,.1,"threshold"}, 0, .5}]
 
 
 
