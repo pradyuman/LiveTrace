1738afb43e0fcd394f286aa0e288e7062508bf76
tsatter <tasat@me.com>
2015-03-07 18:55:29 -0500

    First Upload for Unity/Oculus Code

---
+++ Oculus/OculusWorldDemo/Assets/OVR/Scripts/Util/WebcamTexture.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamTexture : MonoBehaviour {
+
+	WebCamDevice[] devices;
+	WebCamTexture myCam;
+
+
+	Texture curTexture;
+	Renderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+		// Get webcam devices
+		devices = WebCamTexture.devices;
+		myCam = new WebCamTexture();
+		for (var i = 0; i < devices.Length; i++) {
+			Debug.Log (devices [i].name);
+		}
+				
+		myCam.deviceName = devices [0].name;
+		myCam.Play();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		myRenderer = GetComponent<Renderer> ();
+
+		myRenderer.material.SetTexture(0, myCam);
+
+
+	}
+}
