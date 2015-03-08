6c6ea8cd7f4218fb2093841269402720eff501b9
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-08 01:20:01 -0500

    Update imageProcessing.m

--- Wolfram/imageProcessing.m
+++ Wolfram/imageProcessing.m
@@ -6,7 +6,7 @@ color = White;
 
 (*url = ToExpression[Rest[$ScriptCommandLine]];*)
 Manipulate[
-	RemoveBackground[
-	ColorNegate[
-	EdgeDetect[
- Import["/Users/Pradyuman/Documents/LiveTrace/images/purdue_campus.jpg"],r,t]], {"Background", color}],{{r,2,"radius"},1,10},{{t,.1,"threshold"}, 0, .5}]
+  RemoveBackground[
+    ColorNegate[
+      EdgeDetect[
+        Import["/Users/Pradyuman/Documents/LiveTrace/images/purdue_campus.jpg"],r,t]], {"Background", color}],{{r,2,"radius"},1,10},{{t,.1,"threshold"}, 0, .5}]
