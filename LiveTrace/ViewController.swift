350751134d05702dbc15eb2c1c23cca857cf6dca
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-07 20:45:40 -0500

    Saved to disk and updated documentation

--- LiveTrace/ViewController.swift
+++ LiveTrace/ViewController.swift
@@ -18,37 +18,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
    }
-
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-
+   
+   //Open camera when button is pressed
    @IBAction func cameraButton(sender: AnyObject) {
       let imagePicker = UIImagePickerController()
+      //If camera is available, open camera
       if UIImagePickerController.isSourceTypeAvailable(.Camera){
          imagePicker.sourceType = .Camera
       }
-      else{
+      //If camera is not available then open Photo Library
+      else {
          imagePicker.sourceType = .PhotoLibrary
       }
       
       //Self delegate
       imagePicker.delegate = self
       
+      //Present the camera app
       presentViewController(imagePicker, animated: true, completion: nil)
    }
    
+   //Takes the image from the camera -> displays it on the phone and uploads it to iCloud
    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+      //pickedPhoto is the photo taken
       let pickedPhoto = info[UIImagePickerControllerOriginalImage] as UIImage
       
       takenImage = pickedPhoto
       
+      //Dismiss Camera
       dismissViewControllerAnimated(true, completion: nil)
       
       if takenImage != nil {
+         //Display image on the iPhone after it is taken
          myImageView.image = takenImage
-      
+         //Save image to iPhone
+         saveToDisk(takenImage);
       }
       
    }
